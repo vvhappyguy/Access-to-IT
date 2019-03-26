db93439356227597f6e7c37a97bd063a6001cfef
Vitaly Ruzanov <vp.ruzanov@gmail.com>
2019-03-26 22:36:20 +0300

    Added task №7

---
+++ CppStudio/Beginner/07.cpp
@@ -0,0 +1,12 @@
+//compiled with clang (10.0.1)
+//Прямоугольный треугольник в С++
+//Output: a triagle made out of plus symbols 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "+\n++\n+++\n++++\n+++++\n++++++\n+++++++\n";
+    return 0;
+}
