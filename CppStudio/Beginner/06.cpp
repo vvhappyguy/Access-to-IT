d730cec1eecc919d91b575d3796ded42257eeb01
Ivan Dyakin <vvhappyguy@gmail.com>
2019-03-31 22:15:33 +0300

    Added task 6.

---
+++ CppStudio/Beginner/06.cpp
@@ -0,0 +1,21 @@
+// Compile with g++ (gcc ver. 8.2.0)
+// 6. Программно нарисовать домик в С++ - I
+// Input: no input data
+// Output: console picture of house.
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+ cout << "\t    /  \\\n"
+    << "\t   /    \\\n"
+    << "\t  /------\\\n"
+    << "\t / |    | \\\n"
+    << "\t   |    |   \n"
+    << "\t   |    |    \n"
+    << "\t   |____|     \n";
+ 
+ return 0;
+}
