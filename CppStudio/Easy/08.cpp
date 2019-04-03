6490b0591017eb9f6e98eb30e52b3a162a7c216f
Nikita Chubarov <qNickq@bk.ru>
2019-04-04 02:02:26 +0300

    Added task 8.

---
+++ CppStudio/Easy/08.cpp
@@ -0,0 +1,38 @@
+//Compile with g++ (ver. 8.1.0)
+//Task : Сalculate function value : z(x,m) = x^m*sin^m(xm)
+//     For m from 1 to 5 with step 1.
+//     For x from -1.1 to 0.3 with step 0.2.
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+    double z = 1;
+
+    for (double x = -1.1; x <= 0.3; x += 0.2)
+    {
+        cout << "When x = " << x << endl;
+
+        for(int m = 1; m < 5; ++m)
+        {
+            double tempX = 1;
+            double tempSIN = 1;
+
+            for (int i = 0; i < m; ++i)
+            {
+                tempX *= x;
+                tempSIN *= sin(x*m); 
+            }
+
+            z = tempX * tempSIN;
+            cout << "Function value = " << z << ". When m = " << m << ".\n";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
