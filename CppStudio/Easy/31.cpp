4b76ea60f4c2e5a5397b07f539505565c7ade695
Vitaly Ruzanov <vp.ruzanov@gmail.com>
2019-04-05 21:21:38 +0300

    Added task E31

---
+++ CppStudio/Easy/31.cpp
@@ -0,0 +1,20 @@
+//compiled with clang (10.0.1)
+//31. Циклическое вычисление суммы чисел
+//Input: int 
+//Output: int 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int lastNumber, sum = 0;
+    cout << "Введите последнее число последовательности: ";
+    cin >> lastNumber;
+    for (int i = 5; i <= lastNumber; i += 5)
+    {
+        sum += i;
+    }
+    cout << "Сумма чисел, делящихся на 5, из последовательности [0, " << lastNumber << "] равна " << sum << endl;
+    return 0;
+}
