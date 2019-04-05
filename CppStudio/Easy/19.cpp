a12b021be96842a1048449d7ae4605b49247a218
Vitaly Ruzanov <vp.ruzanov@gmail.com>
2019-04-05 16:53:04 +0300

    Added task E19.

---
+++ CppStudio/Easy/19.cpp
@@ -0,0 +1,33 @@
+//compiled with clang (10.0.1)
+//19. Программа палиндром в С++
+//Input: string
+//Output: string
+
+#include <iostream> 
+#include <string>
+using namespace std;
+
+bool palindr(string word)
+{
+    if (word.length() == 1 || word.length() == 0) return true;
+    if (word.front() != word.back()) return false;
+    word.pop_back();
+    word.erase(0,1);
+    return palindr(word);
+}
+
+int main(int argc, char* argv[])
+{
+    string word;
+    cout << "Enter the word: ";
+    cin >> word;
+    if (palindr(word)) 
+    {
+        cout << "The word is a palindrome" << endl;
+    }
+    else 
+    {
+        cout << "The word is not a palindrome" << endl;
+    }
+    return 0;
+}
