912161a001cf5793618a4be067e60aadfca8955e
Nikita Chubarov <qNickq@bk.ru>
2019-04-06 18:59:33 +0300

    Added task E25

---
+++ CppStudio/Easy/25.cpp
@@ -0,0 +1,41 @@
+//Compile with g++ (ver. 8.1.0)
+//Task : Find the sum of the numbers totally divisible by 5.
+//Inpuf : int
+//Output : int
+
+#include <iostream>
+
+using namespace std;
+
+
+int main (int argc , char* argv[])
+{
+    int number;
+
+    int summ = 0;
+    int temp = 0;
+
+    cout << "Enter a number : ";
+    cin >> number;
+    
+
+    if(number < 0)
+    {
+        while (temp >= number)
+        {
+            summ += temp;
+            temp -= 5; 
+        }
+        
+    }
+    else
+    {
+        while (temp <= number)
+        {
+            summ += temp;
+            temp += 5; 
+        }
+    }
+    cout << "Summ = " << summ;
+    return 0;
+}
