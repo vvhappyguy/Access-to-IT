086e8ec29573ede80de165e5e6debaa836929400
Ivan <vvhappyguy@gmail.com>
2019-04-06 13:59:46 +0300

    Added task E18

---
+++ CppStudio/Easy/18.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Program take dec-number in hex format." << endl;
+
+	int number = 0;
+	cout << "Enter number: ";
+	cin >> number;
+	cout << "Number " << number << " in hex means " << hex << number << endl;
+	return 0;
+}
