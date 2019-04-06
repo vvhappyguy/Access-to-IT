23dc8261d060f12fd6c9c956aa7321d870ff3c75
Nikita Chubarov <qNickq@bk.ru>
2019-04-06 17:31:11 +0300

    Added task E29

---
+++ CppStudio/Easy/29.cpp
@@ -0,0 +1,28 @@
+//Compile with g++ (ver. 8.1.0)
+//Task : Determine that the number is integer.
+//Inpuf : double
+//Output : string
+
+#include <iostream>
+
+using namespace std;
+
+
+int main (int argc , char* argv[])
+{
+    double number;
+    cout << "Enter a number : ";
+    cin >> number;
+    
+    
+    if( int(number) != number )
+    {
+        cout << "This number is not integer!";
+    }
+    else
+    {
+        cout << "This number is integer!";
+    }
+    
+    return 0;
+}
