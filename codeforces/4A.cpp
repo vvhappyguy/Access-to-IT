14fbf0da8f9774779e2af0b2266777cc170214ae
Ivan Dyakin <vvhappyguy@gmail.com>
2020-05-03 16:17:29 +0300

    Added solution for tasks 4A and 71A from codeforces.

---
+++ codeforces/4A.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemset/problem/4/A
+#include <iostream>
+
+int main()
+{
+    int weight = 0;
+    std::cin >> weight;
+    std::cout << (((weight>2)&&(weight % 2 == 0))?"YES":"NO");
+    return 0;
+}
---
+++ codeforces/71A.cpp
@@ -0,0 +1,28 @@
+// https://codeforces.com/problemset/problem/71/A
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    int count = 0;
+    std::cin >> count;
+    std::vector<std::string> words;
+    for(int i = 0; i < count; ++i)
+    {
+        std::string word;
+        std::cin >> word;
+        if(word.size() <= 10)
+        {
+            words.push_back(word);
+        }
+        else
+        {
+            std::string long_word = word[0] + std::to_string(word.size()-2) + word[word.size()-1];
+            words.push_back(long_word);
+        }
+    } 
+    for(const auto& word: words)
+        std::cout << word << std::endl;
+    return 0;
+}
