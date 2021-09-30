3f218668072c4764b9000be43d7475bb72f7330f
Ivan Dyakin <vvhappyguy@gmail.com>
2021-10-01 02:31:53 +0300

    Added solution for problem 1 in cpp.
    Updated folder structure.

---
+++ leetcode/1/1.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/two-sum/
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+
+        unordered_map<int, int> hashMap;
+        int currentIndex = 0;
+        for(const auto& num: nums)        
+        {
+            auto it = hashMap.find(num);
+            if(it != hashMap.end())
+            {
+                return {it->second, currentIndex};               
+            }
+            else
+            {
+                hashMap[target - num] = currentIndex;
+            }
+            currentIndex++;
+        }
+    return result;
+}
+
+void printVector(const vector<int>&nums)
+{
+    for(auto& num: nums)
+    {
+        std::cout << num << " ";
+    }
+}
+
+int main(int args, char* argv[]){
+    vector<int> t1 = {1,2,3,4,5,6};
+    int target;
+    std::cin >> target;
+    printVector(twoSum(t1, target));
+}
